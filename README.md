f56a6578929951295b5223bc3c0e3d5a6536b945
alan.fritsch <alan.fritsch@sap.com>
2020-06-26 00:47:16 -0300

    Adiciona pequenas melhorias na legibilidade do codigo e breves melhorias de funcionalidade

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # outrun
 Simple project using JS P5 library to create a game similar to an arcade Outrun.
+
+
+Done:
+==========
+11/03/2018
+- Desenhar pista (grama, borda e rua)
+
+12/03/2018
+- Movimentar na pista (frente, traz, esquerda e direita)
+
+15/03/2018
+- Alteração na lógica de renderização, ao invés de criar os objetos e as linhas na hora
+	de renderizar, cria um array de linhas que serão desenhadas depois.
+- Agora a pista possui curvas para esquerda e direita.
+- Agora a pista possui subidas e descidas.
+- Adicionado renderizar arvores, arbustos e outros (com bug)
+
+16/03/2018
+- Corrigido bug de renderização da pista que fazia após qualquer morro apresentar pedaços
+	perdidos de pista.
+- Corrigido bug de renderização da pista quando chegava ao limite dela até o recomeço.
+
+25/06/2020
+- Identação e melhorias de código para facilitar a leitura do mesmo
+
+
+To do:
+======
+- Adicionar velocidade
+- Corrigir apresentação de 'sprites' (arvores e outros) para deixar num tamanho melhor e
+	movimentando conforme se chega mais próximo.
+- Adicionar 3 faixas de rodagem na pista.
+- Adicionar arrasto para ficar fora da pista nas curvas se não corrigir a direção.
+- Adicionar carro na pista
+- Considerar colisão
+- Exibir outros veículos na pista.
--- sketch.js
+++ sketch.js
@@ -1,23 +1,25 @@
 var width;
 var height;
-var roadW;
-var segL; //segment length
-var camD; //camera depth
+var roadWidth;
+var segmentLength;
+var cameraDepth;
 
 var img;
 var canvas;
 
-var grass1;
-var grass2;
-var rumble1;
-var rumble2;
-var road1;
-var road2;
+var grassEven;
+var grassOdd;
+var rumbleEven;
+var rumbleOdd;
+var roadEven;
+var roadOdd;
 
-var pos;
+var currentPosition;
 var startPos;
 var playerX;
 var speed;
+var maxSpeed = 3;
+var minSpeed = 0;
 
 var lines = [];
 
@@ -42,60 +44,57 @@ class Line {
 		this.sprite = null;
 		this.originalSpriteWidth;
 		this.originalSpriteHeight;
-	}	
+	}
+
 	project(camX, camY, camZ) {
-		this.scale = camD/(this.z-camZ);
+		this.scale = cameraDepth/(this.z-camZ);
     	this.X = (1 + this.scale *(this.x - camX)) * width/2;
     	this.Y = (1 - this.scale * (this.y - camY)) * height/2;
-    	this.W = this.scale * roadW  * width/2;
+    	this.W = this.scale * roadWidth  * width/2;
   	}			
 			
   	drawSprite(correction) {
   		if (this.sprite != null) {
-
-    	var s = this.sprite;
-    	
-    	var w = this.originalSpriteWidth;
-    	var h = this.originalSpriteHeight;
-// 		console.log(w +' -- '+h);
-// 		console.log(' '+this.X +' '+ this.scale +' '+ this.spriteX +' '+ width);
-    	var destX = this.X + this.scale * this.spriteX * width/2;
-    	
-    	var destY = this.Y;
-    	var destW  = w * this.W / 266;
-    	var destH  = h * this.W / 266;
-    	
-    	
-
-		console.log('destW: '+ destW + ' destH: '+ destH +' -- W: '+ this.W +'   destX: '+destX +' destY:'+ destY );
-//     	destX += destW * this.spriteX + playerX*roadW - correction ; //offsetX
-    	destY -= (destH/5);
-//     	destY += destH * (-1);    //offsetY
-destX = (this.X - this.W) + (this.scale*this.spriteX);
-    	console.log('** destX: '+destX +' destY:'+ destY );
-    	
-      	//console.log('destW: '+ destW + ' destH: '+ destH +' -- W: '+ this.W +'   destX: '+destX +' destY:'+ destY );		
-
-    	var clipH = destY+destH-this.clip;
-    	if (clipH<0) clipH=0;
+			var s = this.sprite;
 			
-    	if (clipH>=destH) {
-    		s.hide();
-    	} else {
-    	
-//     	s.setTextureRect(IntRect(0,0,this.w,this.h-this.h*clipH/destH));
-//     	s.size(destW/w,destH/h);
-		s.size(50,50);
-    	s.position(destX, destY);
-    	s.show();
-    	}
+			var w = this.originalSpriteWidth;
+			var h = this.originalSpriteHeight;
+			// console.log(w +' -- '+h);
+			// console.log(' '+this.X +' '+ this.scale +' '+ this.spriteX +' '+ width);
+			var destX = this.X + this.scale * this.spriteX * width/2;
+			
+			var destY = this.Y;
+			var destW  = w * this.W / 266;
+			var destH  = h * this.W / 266;
+			
+			// console.log('destW: '+ destW + ' destH: '+ destH +' -- W: '+ this.W +'   destX: '+destX +' destY:'+ destY );
+			// destX += destW * this.spriteX + playerX*roadWidth - correction ; //offsetX
+			destY -= (destH/5);
+			// destY += destH * (-1);    //offsetY
+			destX = (this.X - this.W) + (this.scale*this.spriteX);
+			// console.log('** destX: '+destX +' destY:'+ destY );
+
+			var clipH = destY+destH-this.clip;
+			if (clipH<0) {
+				clipH=0;
+			}
+				
+			if (clipH>=destH) {
+				s.hide();
+			} else {    	
+				// s.setTextureRect(IntRect(0,0,this.w,this.h-this.h*clipH/destH));
+				// s.size(destW/w,destH/h);
+				s.size(50,50);
+				s.position(destX, destY);
+				s.show();
+			}
     	}
     }
 };
 
 //draw a rectangle line
-function drawQuad(c, x1, y1, w1, x2, y2, w2) {
-	fill(c);
+function drawQuad(color, x1, y1, w1, x2, y2, w2) {
+	fill(color);
 	strokeWeight(0);
 	//console.log(x1-w1+'-'+y1+'-'+x2-w2+'-'+y2+'-'+x2+w2+'-'+y2+'-'+x1+w1+'-'+y1);
   	quad(x1-w1,y1,x2-w2,y2,x2+w2,y2,x1+w1,y1); 	
@@ -105,171 +104,187 @@ function preload() {
 //   img = loadImage('img/bg.png');
 }
 
-function createHiddenImage(path, l) {
+function createHiddenImage(path, lineNumber) {
 	var newImg = createImg(path);
 	//newImg.show();
 	console.log(newImg.width +' *** '+ newImg.height);
 // 	l.originalSpriteWidth=newImg.width;
 // 	l.originalSpriteHeight=newImg.height;
-	l.originalSpriteWidth=1024;
-	l.originalSpriteHeight=372;	
+	lineNumber.originalSpriteWidth=1024;
+	lineNumber.originalSpriteHeight=372;	
 	
 	newImg.hide();
-	l.sprite=newImg;
+	lineNumber.sprite=newImg;
 	count++;
 	return newImg;
 }
 
 function setup() {
-	roadW = 1700;
-// 	roadW = 1700/5;
-	segL = 200; //segment length
-	camD = 0.84;
+	roadWidth = 1700;
+	segmentLength = 200;
+	cameraDepth = 0.84;
 	width = 1024;
-	height = 700; //768;
+	height = 700;
 	
-	grass1  = color(16, 200, 16);
-	grass2  = color(0,154,0);
-	rumble1 = color(255, 255, 255);
-	rumble2 = color(150,0,30);
-	road1   = color(107, 107, 107);
-	road2   = color(105,105,105);
+	grassEven  = color(16, 200, 16);
+	grassOdd  = color(0,154,0);
+	rumbleEven = color(255, 255, 255);
+	rumbleOdd = color(150,0,30);
+	roadEven   = color(107, 107, 107);
+	roadOdd   = color(105,105,105);
 	
 	canvas = createCanvas(width, height);
 	canvas.position(0,0);
 	
-	pos = 0;
-// 	pos = 204000;
+	speed = 0;
+	currentPosition = 0;
+// 	currentPosition = 204000;
 	playerX = 0;
 	curve = 0;
 	
  	img = createImg("img/bg.png");
-	
 	img.position(0,0);
-// 	img.size(width, 400);
 	img.size(width, 372);	
 	
 	// sprite5 = createHiddenImage("img/5.png");
-//  	sprite6 = createHiddenImage("img/6.png");
-//  	sprite4 = createHiddenImage("img/4.png");
-//  	sprite1 = createHiddenImage("img/1.png");
-//  	sprite7 = createHiddenImage("img/7.png");
-// 	
-	//
+ 	// sprite6 = createHiddenImage("img/6.png");
+ 	// sprite4 = createHiddenImage("img/4.png");
+ 	// sprite1 = createHiddenImage("img/1.png");
+ 	// sprite7 = createHiddenImage("img/7.png");
+
 	for(i=0;i<1600;i++) {
-       line = new Line();
-       line.z = i*segL;
+		line = new Line();
+		line.z = i*segmentLength;
 
-       if (i>300 && i<700) line.curve=0.5;
-       if (i>1100 && i<1400) line.curve=-0.7;
+		if (i>300 && i<700) line.curve=0.5;
+		if (i>1100 && i<1400) line.curve=-0.7;
 
-       // if (i<300 && i%20==0) {line.spriteX=-2.5; line.sprite=sprite5;}
-//        if (i%17==0)          {line.spriteX=2.0; line.sprite=sprite6;}
-//        if (i>300 && i%20==0) {line.spriteX=-0.7; line.sprite=sprite4;}
-//        if (i>800 && i%20==0) {line.spriteX=-1.2; line.sprite=sprite1;}
-//        if (i==400)           {line.spriteX=-1.2; line.sprite=sprite7;}
+		// if (i<300 && i%20==0) {line.spriteX=-2.5; line.sprite=sprite5;}
+		// if (i%17==0)          {line.spriteX=2.0; line.sprite=sprite6;}
+		// if (i>300 && i%20==0) {line.spriteX=-0.7; line.sprite=sprite4;}
+		// if (i>800 && i%20==0) {line.spriteX=-1.2; line.sprite=sprite1;}
+		// if (i==400)           {line.spriteX=-1.2; line.sprite=sprite7;}
 
-// 	   if (i<300 && i%20==0) {line.spriteX=-2.5; line.sprite=createHiddenImage("img/5.png");}
-//        if (i%17==0)          {line.spriteX=2.0; line.sprite=createHiddenImage("img/6.png");}
-//        if (i>300 && i%20==0) {line.spriteX=-0.7; line.sprite=createHiddenImage("img/4.png")}
-//        if (i>800 && i%20==0) {line.spriteX=-1.2; line.sprite=createHiddenImage("img/1.png")}
-//        if (i==400)           {line.spriteX=-1.2; line.sprite=createHiddenImage("img/7.png")}
+		// if (i<300 && i%20==0) {line.spriteX=-2.5; line.sprite=createHiddenImage("img/5.png");}
+		// if (i%17==0)          {line.spriteX=2.0; line.sprite=createHiddenImage("img/6.png");}
+		// if (i>300 && i%20==0) {line.spriteX=-0.7; line.sprite=createHiddenImage("img/4.png")}
+		// if (i>800 && i%20==0) {line.spriteX=-1.2; line.sprite=createHiddenImage("img/1.png")}
+		// if (i==400)           {line.spriteX=-1.2; line.sprite=createHiddenImage("img/7.png")}
 
-	   if (i<300 && i%20==0) {line.spriteX=-2.5;}
-       if (i%17==0)          {line.spriteX=2.0;}
-       if (i>300 && i%20==0) {line.spriteX=-0.7;}
-       if (i>800 && i%20==0) {line.spriteX=-1.2;}
-       if (i==400)           {line.spriteX=-1.2;}
-
-
-       if (i>750&i<1410) line.y = sin(i/30.0)*1500;
+		if (i<300 && i%20==0) {
+			line.spriteX=-2.5;
+		}
+		if (i%17==0 ) {
+			line.spriteX = 2.0;
+		}
+		if (i>300 && i%20==0) {
+			line.spriteX=-0.7;
+		}
+		if (i>800 && i%20==0) {
+			line.spriteX=-1.2;
+		}
+		if (i==400) {
+			line.spriteX=-1.2;
+		}
+		if (i>750 & i<1410) {
+			line.y = sin(i / 30.0) * 1500;
+		}
        
-       lines.push(line);
-     }
-     
-     imgTemp=createHiddenImage("img/7.png", lines[100]);
-     lines[100].spriteX=-1.2
-     
-     
-//      frameRate(50);
-    noLoop();
+		lines.push(line);
+	}
+	
+	// Temp to evaluate the behaviour ouf the sprites
+    imgTemp = createHiddenImage("img/7.png", lines[300]);
+	lines[300].spriteX = -1.2
+
+    frameRate(20);
+    // noLoop();
 }
 
 function draw() { 
-  	maxy = height;
+	maxy = height;
   	N = lines.length;
   	x = 0;
   	dx = 0;
   	H = 1500;
   	
-  	if(pos/segL > H) {
-  		//console.log('Max pos = '+pos);
-  		pos=0;
-  	}
-  	
-  	startPos = pos/segL;
-  	
+	if (currentPosition/segmentLength > H) {
+		console.log('Max pos = '+currentPosition);
+		currentPosition=0;
+	}
+	
+	currentPosition += (speed * segmentLength);
+	startPos = currentPosition/segmentLength;
+
   	camH = lines[startPos].y + H;
 
-//   	if (keyIsDown(LEFT_ARROW)) {
-// 		playerX -= 1;
-// 	}
-// 	if (keyIsDown(RIGHT_ARROW)) {
-//     	playerX += 1;
-//   	}
-// 	if (keyIsDown(UP_ARROW)) {
-// 		pos += 200;
-//   	}
-// 	if (keyIsDown(DOWN_ARROW)) {
-//     	pos -= 200;
-// 	}
-	clear();
+  	if (keyIsDown(LEFT_ARROW)) {
+		playerX -= 1;
+	}
+	if (keyIsDown(RIGHT_ARROW)) {
+    	playerX += 1;
+  	}
+	if (keyIsDown(UP_ARROW)) {
+		currentPosition += segmentLength;
+		// speed += 1;
+		// if (speed >= maxSpeed) {
+		// 	speed = maxSpeed;
+		// }
+  	}
+	if (keyIsDown(DOWN_ARROW)) {
+		currentPosition -= segmentLength;
+		// speed -= 1;
+		// if (speed <= minSpeed) {
+		// 	speed = minSpeed;
+		// }
+	}
 
-// 	drawQuad(road1,   255, 512, 3655,      512, 1452, 1827);
-// 	drawQuad(road2,   1452, 512, 1827,      512, 1085, 1218);
-// 	drawQuad(road1,   1085, 512, 1218,      512, 901, 913);
+	clear();
 
 	for (n=startPos+1; n<startPos+300; n++) {
 		l = lines[n%N];
-     	l.project(playerX*roadW/5-x, camH, (startPos*segL - (n>=N?N*segL:0)));
-//      	console.log(' aa '+ playerX*(roadW/5)-x +'-'+ camH +' - '+ (startPos*segL - (n>=N?N*segL:0)));
-     	x+=dx;
-    	dx+=l.curve;
+     	l.project(playerX*roadWidth/5-x, camH, (startPos*segmentLength - (n>=N?N*segmentLength:0)));
+//      	console.log(' aa '+ playerX*(roadWidth/5)-x +'-'+ camH +' - '+ (startPos*segmentLength - (n>=N?N*segmentLength:0)));
+     	x += dx;
+    	dx += l.curve;
 
-		l.clip=maxy;
-    	if (l.Y>=maxy){ 
+		l.clip = maxy;
+    	if (l.Y >= maxy) { 
     		//alert('maxy');
     		//console.log('maxy ='+ maxy);
-    		} else {
-    	maxy = l.Y;
+		} else {
+    		maxy = l.Y;
 		
-		grass  = n%2 ? grass1:grass2;
-		rumble = n%2 ? rumble1:rumble2;
-		road   = n%2 ? road1:road2;
+			grass  = n%2 ? grassEven:grassOdd;
+			rumble = n%2 ? rumbleEven:rumbleOdd;
+			road   = n%2 ? roadEven:roadOdd;
 
-		p = lines[n-1%N];
-				
-		if (p != null) {
-		drawQuad(grass,    0, p.Y, width,    0,   l.Y, width);
-		drawQuad(rumble, p.X, p.Y, p.W*1.18,  l.X, l.Y, l.W*1.18);
-		drawQuad(road,   p.X, p.Y, p.W,      l.X, l.Y, l.W);
-// 		console.log('road   '+ round(p.X) +'-'+ round(p.Y)+'-'+ round(p.W)+'    -    '+ round(l.X)+'-'+ round(l.Y)+'-'+ (l.W))
-		}
-		l.drawSprite(x);
+			p = lines[n-1%N];
+					
+			if (p != null) {
+				drawQuad(grass,    0, p.Y, width,    0,   l.Y, width);
+				drawQuad(rumble, p.X, p.Y, p.W*1.18,  l.X, l.Y, l.W*1.18);
+				drawQuad(road,   p.X, p.Y, p.W,      l.X, l.Y, l.W);
+				// console.log('road   '+ round(p.X) +'-'+ round(p.Y)+'-'+ round(p.W)+'    -    '+ round(l.X)+'-'+ round(l.Y)+'-'+ (l.W))
+			}
+
+			l.drawSprite(x);
 		}	
 	}
+
+	// currentPosition += speed;
 }
 
-function keyPressed() {
-	//alert('pressed');
-  if (keyCode == LEFT_ARROW) {
-    playerX -= 1;
-  } else if (keyCode == RIGHT_ARROW) {
-    playerX += 1;
-  } else if (keyCode == UP_ARROW) {
-    pos += 200;
-  } else if (keyCode == DOWN_ARROW) {
-    pos -= 200;
-  }
-  redraw();
-}
+// function keyPressed() {
+// 	// alert('pressed');
+// 	if (keyCode == LEFT_ARROW) {
+//     	playerX -= 1;
+//   	} else if (keyCode == RIGHT_ARROW) {
+//     	playerX += 1;
+//   	} else if (keyCode == UP_ARROW) {
+// 		currentPosition += 200;
+// 	} else if (keyCode == DOWN_ARROW) {
+// 		currentPosition -= 200;
+// 	}
+// 	redraw();
+// }
